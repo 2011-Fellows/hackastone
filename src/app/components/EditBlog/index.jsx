8f408d5016f98341fbe8d81968cb31d8562799cf
Mike <maumike87@gmail.com>
2021-02-09 17:16:51 -0500

    chore: change prettier settings

--- src/app/components/EditBlog/index.jsx
+++ src/app/components/EditBlog/index.jsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
-import EditorJS from 'react-editor-js';
-import { EDITOR_JS_TOOLS } from '../../../utils/tools';
+import * as React from 'react'
+import EditorJS from 'react-editor-js'
+import { EDITOR_JS_TOOLS } from '../../../utils/tools'
+import styled from 'styled-components/macro'
 
 export function EditBlog() {
   return (
-    <EditorJS holder="hackastone" tools={EDITOR_JS_TOOLS}>
+    <Editor holder="hackastone" tools={EDITOR_JS_TOOLS}>
       <div id="hackastone" />
-    </EditorJS>
-  );
+    </Editor>
+  )
 }
+
+const Editor = styled(EditorJS)`
+  background-color: blue;
+`
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,11 +1,11 @@
 // Central location for all tools used by editor.js
-import Embed from '@editorjs/embed';
-import Paragraph from '@editorjs/paragraph';
-import Code from '@editorjs/code';
-import LinkTool from '@editorjs/link';
-import Quote from '@editorjs/quote';
-import CheckList from '@editorjs/checklist';
-import InlineCode from '@editorjs/inline-code';
+import Embed from '@editorjs/embed'
+import Paragraph from '@editorjs/paragraph'
+import Code from '@editorjs/code'
+import LinkTool from '@editorjs/link'
+import Quote from '@editorjs/quote'
+import CheckList from '@editorjs/checklist'
+import InlineCode from '@editorjs/inline-code'
 
 export const EDITOR_JS_TOOLS = {
   embed: Embed,
@@ -14,5 +14,5 @@ export const EDITOR_JS_TOOLS = {
   linkTool: LinkTool,
   quote: Quote,
   checklist: CheckList,
-  inlineCode: InlineCode,
-};
+  inlineCode: InlineCode
+}
