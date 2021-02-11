9fcf0161cf078d065d8168e4ada0876345ad3845
Michael Maurer <maumike87@gmail.com>
2021-02-11 18:20:50 -0500

    style: style save post button

--- src/app/components/EditBlog/index.tsx
+++ src/app/components/EditBlog/index.tsx
@@ -11,17 +11,15 @@ export function EditBlog() {
     console.log(savedData)
   }
   return (
-    <>
-      <EditorJS
-        holder="hackastone"
-        tools={EDITOR_JS_TOOLS}
-        placeholder="Begin editing here..."
-        instanceRef={(instance) => (instanceRef.current = instance)}
-      >
-        <Div id="hackastone" />
-      </EditorJS>
-      <button onClick={() => handleSave()}>Save Post</button>
-    </>
+    <EditorJS
+      holder="hackastone"
+      tools={EDITOR_JS_TOOLS}
+      placeholder="Begin editing here..."
+      instanceRef={(instance) => (instanceRef.current = instance)}
+    >
+      <Div id="hackastone" />
+      <Button onClick={() => handleSave()}>Save Post</Button>
+    </EditorJS>
   )
 }
 
@@ -31,5 +29,21 @@ const Div = styled.div`
   border: 1px solid #3e3e3e;
   border-radius: 10px;
   box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.15);
-  margin: 6rem auto;
+  margin: 6rem auto 2rem;
+  position: relative;
+`
+
+const Button = styled.a`
+  background-color: #4caf50;
+  border: none;
+  border-radius: 50px;
+  color: #efefef;
+  padding: 1rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+  font-size: 1rem;
+  margin: 1rem auto;
+  box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.15);
 `
