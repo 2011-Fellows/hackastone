29b35b99c12763dcc3903a1ba2599341d559d0cf
Michael Maurer <maumike87@gmail.com>
2021-02-17 11:33:03 -0500

    feat: move editor to dedicated page

--- src/app/pages/EditPage/index.tsx
+++ src/app/pages/EditPage/index.tsx
@@ -1,25 +1,19 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet-async'
 import { NavBar } from 'app/components/NavBar'
-import { Masthead } from './Masthead'
-import { Features } from './Features'
 import { PageWrapper } from 'app/components/PageWrapper'
 import { EditBlog } from 'app/components/EditBlog'
 
-export function HomePage() {
+export function EditPage() {
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React Boilerplate application homepage"
-        />
+        <title>Edit Blog Page</title>
+        <meta name="description" content="Create or edit a blog post" />
       </Helmet>
       <NavBar />
       <PageWrapper>
-        <Masthead />
-        <Features />
+        <EditBlog />
       </PageWrapper>
     </>
   )
