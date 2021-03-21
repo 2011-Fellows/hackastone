89b0a690c20685c455d9a9adbe9e0b437f54677a
HoraceZ <zhanghorace@gmail.com>
2021-03-21 16:24:10 -0400

    Feat: able to load articles from DB, Blocker: content datatype issue

--- src/app/pages/SingleArticle/index.tsx
+++ src/app/pages/SingleArticle/index.tsx
@@ -5,22 +5,59 @@ import { NavBar } from 'app/components/NavBar'
 import { PageWrapper } from 'app/components/PageWrapper'
 import axios from 'axios'
 
-export function SingleArticle() {
-  const [singleArticle, setSingleArticle] = useState({})
+// interface singleArticleData {
+//   category: string
+//   content: string
+//   date: any
+//   id: number
+//   dislikes: number
+//   likes: number
+//   title: string
+//   userId: string
+// }
+
+export function SingleArticle({ match }) {
+  const [singleArticle, setSingleArticle] = useState({
+    category: '',
+    content: '',
+    date: '',
+    id: '',
+    dislikes: '',
+    likes: '',
+    title: '',
+    userId: ''
+  })
 
   useEffect(() => {
-    fetchSingleArticle(1)
-  })
+    fetchSingleArticle(match.params.id)
+  }, [])
 
   const fetchSingleArticle = async (artId) => {
     try {
       const { data } = await axios.get(`/api/articles/${artId}`)
-      console.log('this is the dattaaa: ', data)
-      // setSingleArticle(data)
+      setSingleArticle(data)
+      console.log('in fetch')
     } catch (error) {
       console.log(error)
     }
   }
+  console.log('THIS IS SINGLE ARTICLE', singleArticle)
+
+  // let displayContent = ''
+
+  // const parseContent = (contentData) => {
+  //   const parsedContent = JSON.parse(contentData.content)
+  //   console.log('parsed contentttt', parseContent)
+  //   return parsedContent
+  // }
+  // singleArticle && singleArticle.content
+  //   ? (displayContent = parseContent(singleArticle))
+  //   : (displayContent = 'Loading')
+
+  // CURRENT ISSUE: content is saved as "text", but the data type is an object with nested objects that hold the actual content data. Not sure how we want to display this. Options considered:
+  // 1. Save in DB as alternate data type? Objects shouldn't be stored in DB, and parsing may be an issue
+  // 2. Save text only from editor js, instead of saving entire object?
+  // 3. Neither of these seem like good options.
 
   return (
     <>
@@ -30,9 +67,9 @@ export function SingleArticle() {
       </Helmet>
       <NavBar />
       <PageWrapper>
-        {/* <p>Title: ${singleArticle.title}</p> */}
-        <p></p>
-        <p></p>
+        <p>Title: {singleArticle.title}</p>
+        <p>Category: singleArticle.category</p>
+        <p>Content: {singleArticle.content}</p>
         <button onClick={() => alert('Edit article coming soon!')}>Edit</button>
       </PageWrapper>
     </>
